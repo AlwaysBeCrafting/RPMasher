ea2ea39e9e1eeec488b221563a9452b076c77178
Alexander Prime <rainesabaal@gmail.com>
2016-10-21 17:50:16 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,42 @@
+let express = require( 'express' );
+let http    = require( 'http'    );
+let morgan  = require( 'morgan'  );
+let path    = require( 'path'    );
+let less    = require( 'express-less' );
+
+// let bodyParser = require( 'body-parser' );
+// app.use( bodyParser.json() );
+
+
+let app = express();
+let server = http.Server( app );
+
+
+app.use( morgan( 'dev' ));
+
+app.use( '/css', less( path.join( __dirname, '/less' )));
+
+app.use( '/', express.static(
+	path.join( __dirname, 'public' ),
+	{ extensions: [ 'html' ] }
+));
+
+app.get( '/', (request,response) => {
+	response.sendFile( path.join( __dirname, '/public/editor.html' ));
+});
+
+
+/*
+
+app.route( 'path' )
+.get( getHandler )
+.post( postHandler )
+.delete( etc... )
+
+*/
+
+
+let port = process.env.PORT || 8080;
+
+console.log( `Starting server on port ${port}...` );
+server.listen( port );
